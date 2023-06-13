760180433664aa326552090bb527c7ef1cdd1c75
L1keMagic <karachev.artur@gmail.com>
2023-06-13 14:27:41 +0200

    basic ui

---
+++ CityAirportSearch/SearchAirportsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CityAirportSearch
+//
+//  Created by Артур Карачев on 13.06.2023.
+//
+
+import UIKit
+
+class SearchAirportsViewController: UIViewController {
+
+    @IBOutlet weak var roundedView: UIView!
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
